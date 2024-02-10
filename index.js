24ed1f2df18e832fb44dac6e242268a70c1600ff
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-02-10 14:06:12 +0545

    DB connected, config.env added

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config.env",
+});
+
+async function connectToDatabase() {
+  try {
+    const DB = process.env.DB_STRING.replace(
+      "<password>",
+      process.env.DB_PASSWORD
+    );
+    console.log(DB);
+
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+    });
+
+    console.log("DB connection successful");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connectToDatabase();
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello from server!,We will Laundry Service soon!");
 });
 
 app.listen(port, () => {
