2fd360ec13218c83530600017fbb66152d31cb76
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-02-21 16:52:26 +0545

    inavlid routes handled

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(express.json());
 
 app.use('/api/Customer', customerRoutes);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: false,
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
